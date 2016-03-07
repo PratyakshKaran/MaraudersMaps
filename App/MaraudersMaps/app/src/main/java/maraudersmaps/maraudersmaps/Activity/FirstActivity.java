aa701423c363c70ecc8cfb9ef68058e56369d1a8
Ravi Upadhyay <ravi3054@gmail.com>
2016-03-07 08:25:00 +0530

    Added slider for welcome activity

---
+++ App/MaraudersMaps/app/src/main/java/maraudersmaps/maraudersmaps/Activity/FirstActivity.java
@@ -0,0 +1,55 @@
+package maraudersmaps.maraudersmaps.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import maraudersmaps.maraudersmaps.R;
+
+public class FirstActivity extends FragmentActivity
+{
+    public static final String TAG = "MaraudersMaps";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences preference = getPreferences(MODE_PRIVATE);
+        boolean firstTime = preference.getBoolean("firstTime", true);
+
+        if (firstTime)
+        {
+            Log.i(TAG, "Application being used for first time. Will take the user to introduction");
+            startActivity(new Intent(this, IntroductionActivity.class));
+        }
+        else
+        {
+            Log.i(TAG, "Not the first time start. Checking if user is logged in.");
+            boolean loggedIn = preference.getBoolean("loggedIn", false);
+            if (loggedIn)
+            {
+                Log.i(TAG, "User already logged in. Taking to the home page.");
+                startActivity(new Intent(this, HomeActivity.class));
+            }
+            else
+            {
+                Log.i(TAG, "Taking the user to log in page");
+                startActivity(new Intent(this, LoginActivity.class));
+            }
+        }
+        //startService(new Intent(this, PushLocation.class));
+        setContentView(R.layout.activity_first);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
---
+++ App/MaraudersMaps/app/src/main/java/maraudersmaps/maraudersmaps/Activity/IntroductionActivity.java
@@ -0,0 +1,72 @@
+package maraudersmaps.maraudersmaps.Activity;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import maraudersmaps.maraudersmaps.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+    }
+
+    public class CustomPagerAdapter extends PagerAdapter {
+
+        private Context mContext;
+
+        public CustomPagerAdapter(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup collection, int position) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.pager_item, collection, false);
+
+            int imageId = getResources().getIdentifier(("i" + (position + 1)), "mipmap", getPackageName());
+            ImageView imageView = new ImageView(mContext);
+            imageView.setImageResource(R.mipmap.i1);
+            layout.addView(imageView);
+
+            collection.addView(layout);
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup collection, int position, Object view) {
+            collection.removeView((View) view);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Title";
+        }
+
+    }
+}
